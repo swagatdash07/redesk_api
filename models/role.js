d1ee6007a60b3f54453f37fa68fefa35283f090f
unknown <swagatdash07@gmail.com>
2022-07-14 18:25:40 +0530

    redesk api hosting

--- models/role.js
+++ models/role.js
@@ -1,15 +1,14 @@
-const mongoose=require('mongoose');
-const Schema=mongoose.Schema
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-const roleSchema= new Schema({
-    role_name:{
-        type:String,
-        required: true
-    },
-    role_type:{
-        type:String
-    }
-})
-const Role=new mongoose.model("Role",roleSchema)
-module.exports=Role;
-
+const roleSchema = new Schema({
+  role_name: {
+    type: String,
+    required: true,
+  },
+  role_type: {
+    type: String,
+  },
+});
+const Role = new mongoose.model("Role", roleSchema);
+module.exports = Role;
